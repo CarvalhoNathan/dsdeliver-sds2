66b9e7e3369918eec766a4ddbf4ba6975fbf9676
CarvalhoNathan <fariasnathan75@gmail.com>
2021-01-16 15:09:10 -0300

    Add orders list

--- front-mobile/src/Home/index.tsx
+++ front-mobile/src/Home/index.tsx
@@ -1,24 +1,28 @@
-import { OpenSans_700Bold } from '@expo-google-fonts/open-sans';
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
+import Header from '../Header';
 
 function Home() {
 
+    const navigation = useNavigation();
+    
     const handleOnPress = () => {
-
+        navigation.navigate('Orders');
     }
 
     return (
         <>
+            <Header />
             <View style={styles.container}>
                 <Image source={require('../assets/deliveryman.png')} />
                 <Text style={styles.title}>
                     Acompanhe os pedidos e {'\n'} entregue no prazo!
-            </Text>
+                </Text>
                 <Text style={styles.subTitle}>
                     Receba todos os pedidos do seu {'\n'} restaurante na palma da sua mão
-            </Text>
+                </Text>
             </View>
             <View style={styles.footer}>
                 <RectButton style={styles.button} onPress={handleOnPress}>
